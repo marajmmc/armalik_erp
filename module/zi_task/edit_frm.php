22b8f7b6eabd5751a579a41e7d1cad004b050df8
Kamrul Hasan <saj696@gmail.com>
2015-06-23 15:20:37 +0600

    zi_task modified

--- module/zi_task/edit_frm.php
+++ module/zi_task/edit_frm.php
@@ -33,8 +33,8 @@ $editRow = $db->single_data($tbl . "zi_task", "*", "id", $_POST['rowID']);
                             <select id="territory_id" name="territory_id" class="span5" onchange="load_district_by_territory()">
                                 <option value="">Select</option>
                                 <?php
-                                $sql = "select territory_id as fieldkey, territory_name as fieldtext from $tbl" . "territory_info where zone_id='$user_zone'";
-                                echo $db->SelectList($sql, $editRow['territory_id']);
+                                    $sql = "select territory_id as fieldkey, territory_name as fieldtext from $tbl" . "territory_info where zone_id='$user_zone'";
+                                    echo $db->SelectList($sql, $editRow['territory_id']);
                                 ?>
                             </select>
                         </div>
@@ -48,19 +48,19 @@ $editRow = $db->single_data($tbl . "zi_task", "*", "id", $_POST['rowID']);
                             <select id="district_id" name="district_id" class="span5" onchange="load_distributor_by_district()">
                                 <option value="">Select</option>
                                 <?php
-                                $sql_user_group = "SELECT
-                                            $tbl" . "zilla.zillaid as fieldkey,
-                                            $tbl" . "zilla.zillanameeng as fieldtext
+                                    $sql_user_group = "SELECT
+                                        $tbl" . "zilla.zillaid as fieldkey,
+                                        $tbl" . "zilla.zillanameeng as fieldtext
                                         FROM
-                                            $tbl" . "territory_assign_district
-                                            LEFT JOIN $tbl" . "zilla ON $tbl" . "zilla.zillaid = $tbl" . "territory_assign_district.zilla_id
+                                        $tbl" . "territory_assign_district
+                                        LEFT JOIN $tbl" . "zilla ON $tbl" . "zilla.zillaid = $tbl" . "territory_assign_district.zilla_id
                                         WHERE
-                                            $tbl" . "territory_assign_district.del_status=0
-                                            AND $tbl" . "zilla.visible=0
-                                            AND $tbl" . "territory_assign_district.status='Active'
-                                            AND $tbl" . "territory_assign_district.territory_id='".$editRow['territory_id']."'
-                                            ";
-                                echo $db->SelectList($sql_user_group, $editRow['district_id']);
+                                        $tbl" . "territory_assign_district.del_status=0
+                                        AND $tbl" . "zilla.visible=0
+                                        AND $tbl" . "territory_assign_district.status='Active'
+                                        AND $tbl" . "territory_assign_district.territory_id='".$editRow['territory_id']."'
+                                        ";
+                                    echo $db->SelectList($sql_user_group, $editRow['district_id']);
                                 ?>
                             </select>
                         </div>
@@ -74,14 +74,14 @@ $editRow = $db->single_data($tbl . "zi_task", "*", "id", $_POST['rowID']);
                             <select id="distributor_id" name="distributor_id" class="span5">
                                 <option value="">Select</option>
                                 <?php
-                                $sql = "select distributor_id as fieldkey, distributor_name as fieldtext from $tbl" . "distributor_info where status='Active' AND del_status='0' AND zone_id='$user_zone' AND territory_id='".$editRow['territory_id']."' AND zilla_id='".$editRow['district_id']."' order by distributor_name";
-                                $distributorDropDownArray = $db->return_result_array($sql);
-                                foreach($distributorDropDownArray as $DropDown)
-                                {
-                                    ?>
-                                    <option value="<?php echo $DropDown['fieldkey'];?>" <?php if($DropDown['fieldkey']==$editRow['distributor_id']){echo 'selected';}?>><?php echo $DropDown['fieldtext'];?></option>
-                                <?php
-                                }
+                                    $sql = "select distributor_id as fieldkey, distributor_name as fieldtext from $tbl" . "distributor_info where status='Active' AND del_status='0' AND zone_id='$user_zone' AND territory_id='".$editRow['territory_id']."' AND zilla_id='".$editRow['district_id']."' order by distributor_name";
+                                    $distributorDropDownArray = $db->return_result_array($sql);
+                                    foreach($distributorDropDownArray as $DropDown)
+                                    {
+                                        ?>
+                                        <option value="<?php echo $DropDown['fieldkey'];?>" <?php if($DropDown['fieldkey']==$editRow['distributor_id']){echo 'selected';}?>><?php echo $DropDown['fieldtext'];?></option>
+                                    <?php
+                                    }
                                 ?>
                             </select>
                         </div>
@@ -92,7 +92,15 @@ $editRow = $db->single_data($tbl . "zi_task", "*", "id", $_POST['rowID']);
                             Purchase Order
                         </label>
                         <div class="controls">
-                            <input type="text" class="span5" name="purchase_order" value="<?php echo $editRow['purchase_order'];?>"/>
+                            <select id="purchase_order" name="purchase_order" class="span5">
+                                <option value="">Select</option>
+                                <?php
+                                    $sql = "select purchase_order_id as fieldkey, purchase_order_id as fieldtext from $tbl" . "product_purchase_order_invoice
+                                    where del_status='0' AND territory_id='".$editRow['territory_id']."' AND zilla_id='".$editRow['district_id']."' AND distributor_id='".$editRow['distributor_id']."'
+                                    order by id DESC LIMIT 3";
+                                    echo $db->SelectList($sql, $editRow['purchase_order']);
+                                ?>
+                            </select>
                         </div>
                     </div>
 
@@ -101,7 +109,15 @@ $editRow = $db->single_data($tbl . "zi_task", "*", "id", $_POST['rowID']);
                             Collection
                         </label>
                         <div class="controls">
-                            <input type="text" class="span5" name="collection" value="<?php echo $editRow['collection'];?>"/>
+                            <select id="collection" name="collection" class="span5">
+                                <option value="">Select</option>
+                                <?php
+                                    $sql = "select amount as fieldkey, amount as fieldtext from $tbl" . "distributor_add_payment
+                                    where del_status='0' AND territory_id='".$editRow['territory_id']."' AND zilla_id='".$editRow['district_id']."' AND distributor_id='".$editRow['distributor_id']."'
+                                    order by id DESC LIMIT 3";
+                                    echo $db->SelectList($sql, $editRow['collection']);
+                                ?>
+                            </select>
                         </div>
                     </div>
 
@@ -121,7 +137,20 @@ $editRow = $db->single_data($tbl . "zi_task", "*", "id", $_POST['rowID']);
                         <div class="controls">
                             <textarea name="activities" class="span6"><?php echo $editRow['activities'];?></textarea>
                             <input type="file" name="activities_file" class="span3" />
-                            <div class="span2"><img src="../../system_images/zi_task/<?php echo $editRow['activities_image']?>" /></div>
+                            <?php
+                            if(isset($editRow['activities_image']) && strlen($editRow['activities_image'])>0)
+                            {
+                            ?>
+                                <div class="span2"><img height="100" width="100" src="../../system_images/zi_task/<?php echo $editRow['activities_image']?>" /></div>
+                            <?php
+                            }
+                            else
+                            {
+                            ?>
+                                <div class="span2"><img height="100" width="100" src="../../system_images/zi_task/no_image.jpg" /></div>
+                            <?php
+                            }
+                            ?>
                         </div>
                     </div>
 
@@ -132,7 +161,20 @@ $editRow = $db->single_data($tbl . "zi_task", "*", "id", $_POST['rowID']);
                         <div class="controls">
                             <textarea name="problem" class="span6"><?php echo $editRow['problem'];?></textarea>
                             <input type="file" name="problem_file" class="span3" />
-                            <div class="span2"><img src="../../system_images/zi_task/<?php echo $editRow['problem_image']?>" /></div>
+                            <?php
+                            if(isset($editRow['problem_image']) && strlen($editRow['problem_image'])>0)
+                            {
+                                ?>
+                                <div class="span2"><img height="100" width="100" src="../../system_images/zi_task/<?php echo $editRow['problem_image']?>" /></div>
+                            <?php
+                            }
+                            else
+                            {
+                                ?>
+                                <div class="span2"><img height="100" width="100" src="../../system_images/zi_task/no_image.jpg" /></div>
+                            <?php
+                            }
+                            ?>
                         </div>
                     </div>
 
